bd46f33a8b014ceee8d48b25381a32793d8ffdd8
MohammadSaad21 <87072043+MohammadSaad21@users.noreply.github.com>
2021-07-07 14:58:29 +0200

    Implement promotion validation function

--- PromotionEngine/PromotionEngine/Model/Promotions/Promotion.cs
+++ PromotionEngine/PromotionEngine/Model/Promotions/Promotion.cs
@@ -32,5 +32,17 @@ namespace PromotionEngine.Model.Promotions
             this.Start_date = start_date;
             this.End_date = end_date;
         }
+
+        /// <summary>
+        /// Verify if a promotion is still available
+        /// </summary>
+        /// <returns>A true value if the promotion is valid</returns>
+        public bool PromotionValid()
+        {
+            if (Start_date < DateTime.Now && End_date > DateTime.Now)
+                return true;
+            else
+                return false;
+        }
     }
 }
