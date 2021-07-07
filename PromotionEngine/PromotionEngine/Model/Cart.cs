c84cfcf28bc267a9dc55b2429794d78d388b3ee0
MohammadSaad21 <87072043+MohammadSaad21@users.noreply.github.com>
2021-07-07 14:22:38 +0200

    Added Cart class with parameters and a constructor

---
+++ PromotionEngine/PromotionEngine/Model/Cart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Model
+{
+    public class Cart
+    {
+        /// <summary>
+        /// A dictionary containing product objects with the amount in the cart
+        /// </summary>
+        public Dictionary<Product, int> CartItems { get; set; }
+
+        /// <summary>
+        /// Create  an empty cart
+        /// </summary>
+        /// <returns>An empty dictionary cart</returns>
+        public Cart()
+        {
+            this.CartItems = new Dictionary<Product, int>();
+        }
+    }
+}
