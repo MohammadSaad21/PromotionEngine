91f121be37f438a561b17730b9ccbdfa6e428f2d
MohammadSaad21 <87072043+MohammadSaad21@users.noreply.github.com>
2021-07-07 15:02:07 +0200

    Unit tested promotion validation

---
+++ PromotionEngine/PromotionEngine_Test/Model/Promotions/Promotion_Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PromotionEngine.Model;
+using PromotionEngine.Model.Promotions;
+
+namespace PromotionEngine_Test.Model.Promotions
+{
+    [TestClass]
+    public class Promotion_Test
+    {
+        [TestMethod]
+        public void Promotion_Has_Expired()
+        {
+            var SkuA = new Product("A", 50);
+            var promotion = new AmountPromotion(1, new DateTime(2020, 07, 01), new DateTime(2020, 12, 15), SkuA, 3, 130);
+            Assert.IsFalse(promotion.PromotionValid());
+        }
+
+        [TestMethod]
+        public void Promotion_Is_Active()
+        {
+            var SkuA = new Product("A", 50);
+            var promotion = new AmountPromotion(1, new DateTime(2019, 07, 01), new DateTime(2030, 12, 15), SkuA, 3, 130);
+            Assert.IsTrue(promotion.PromotionValid());
+        }
+    }
+}
