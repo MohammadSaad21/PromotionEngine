002ae1aed2cc42bf175fd9c2dc022f48106f48fc
MohammadSaad21 <87072043+MohammadSaad21@users.noreply.github.com>
2021-07-07 14:43:09 +0200

    Implemented a product add function to the cart

--- PromotionEngine/PromotionEngine/Model/Cart.cs
+++ PromotionEngine/PromotionEngine/Model/Cart.cs
@@ -21,5 +21,22 @@ namespace PromotionEngine.Model
         {
             this.CartItems = new Dictionary<Product, int>();
         }
+
+        /// <summary>
+        /// Adds a product to the cart with a specific amount
+        /// </summary>
+        /// <param name="product">The product to be added to the cart</param>
+        /// <param name="amount">TThe amount of the product to be added</param>
+        public void Add(Product product, int amount)
+        {
+            if (this.CartItems.ContainsKey(product))
+            {
+                int oldAmount = 0;
+                CartItems.TryGetValue(product, out oldAmount);
+                CartItems[product] = oldAmount + amount;
+            }
+            else
+                CartItems.Add(product, amount);
+        }
     }
 }
