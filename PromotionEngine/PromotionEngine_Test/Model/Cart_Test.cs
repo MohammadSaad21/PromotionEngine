243aca4999f706e86879fe54fb782c6e1de29397
MohammadSaad21 <87072043+MohammadSaad21@users.noreply.github.com>
2021-07-07 14:52:17 +0200

    Create a unit test for the cart

---
+++ PromotionEngine/PromotionEngine_Test/Model/Cart_Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PromotionEngine.Model;
+
+namespace PromotionEngine_Test.Model
+{
+    [TestClass]
+    public class Cart_Test
+    {
+        /// <summary>
+        /// Create an empty cart and verify the count is zero
+        /// </summary>
+        [TestMethod]
+        public void Create_Empty_Cart()
+        {
+            Cart cart = new Cart();
+            Assert.AreEqual(0, cart.GetTotalCount());
+        }
+
+        /// <summary>
+        /// Addidng two product type items and verifying the result
+        /// </summary>
+        [TestMethod]
+        public void Create_Cart_With_Products()
+        {
+            Cart cart = new Cart();
+            cart.Add(new Product("A", 50), 5);
+            cart.Add(new Product("B", 20), 10);
+
+            Assert.AreEqual(2, cart.GetTotalCount());
+        }
+    }
+}
