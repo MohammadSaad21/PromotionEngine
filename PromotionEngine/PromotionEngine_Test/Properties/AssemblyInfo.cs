fdf53396051270d9a69883ee34669e06abdcd087
MohammadSaad21 <87072043+MohammadSaad21@users.noreply.github.com>
2021-07-07 13:40:26 +0200

    Unit test project created
    A unit test project has been created in this stage to be prepared to begin testing the modules continuously as the project progress on.

---
+++ PromotionEngine/PromotionEngine_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PromotionEngine_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PromotionEngine_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("276e785b-ee71-49d6-87d7-f38685638c7e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
