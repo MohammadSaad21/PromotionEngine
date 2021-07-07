47c076a69fd20bea686a8db70e25fc615a45e483
MohammadSaad21 <87072043+MohammadSaad21@users.noreply.github.com>
2021-07-07 14:25:22 +0200

    Added a base promotion class
    A base promotion class that contains common parameters and contrusctor for all the other promotion types

---
+++ PromotionEngine/PromotionEngine/Model/Promotions/Promotion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Model.Promotions
+{
+    public class Promotion
+    {
+        /// <summary>
+        /// A unique Id for the promotion
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// A start date for the promotion
+        /// </summary>
+        public DateTime Start_date { get; set; }
+
+        /// <summary>
+        /// An end date for the promotion
+        /// </summary>
+        public DateTime End_date { get; set; }
+
+        /// <summary>
+        /// Base promotion constructur for all the promotion types
+        /// </summary>
+        public Promotion(int id, DateTime start_date, DateTime end_date)
+        {
+            this.Id = id;
+            this.Start_date = start_date;
+            this.End_date = end_date;
+        }
+    }
+}
