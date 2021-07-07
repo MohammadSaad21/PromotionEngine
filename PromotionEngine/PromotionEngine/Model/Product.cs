645b8a010b9f97f89ce45fb824a3663f30b5b8c2
MohammadSaad21 <87072043+MohammadSaad21@users.noreply.github.com>
2021-07-07 14:00:35 +0200

    Added Product class with getters, setters and a constructor
    A Product class is created and added inside the Model package.

---
+++ PromotionEngine/PromotionEngine/Model/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Model
+{
+    public class Product
+    {
+        /// <summary>
+        /// A Sku Id for identitifying a specific product
+        /// </summary>
+        public string Sku { get; set; }
+
+        /// <summary>
+        /// A unit price for the product
+        /// </summary>
+        public decimal Unit_price { get; set; }
+
+        /// <summary>
+        /// A product constructur
+        /// </summary>
+        /// <returns>An product object containing Sku and unit price</returns>
+        public Product(string sku, decimal unit_price)
+        {
+            this.Sku = sku;
+            this.Unit_price = unit_price;
+        }
+    }
+}
